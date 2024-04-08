7b0bffe62ebf992ba05f5f81697642b969f85bd3
Ángel Fernández <alfgento@gmail.com>
2024-04-08 09:57:40 +0200

    3rd commit: added new configuration for database

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const dbConnection = require('./config/config')
 const app = express()
 require('dotenv').config()
 PORT = process.env.PORT || 3000
 
 app.get('/',(req, res) => (res.send('Proyecto back')))
+
+dbConnection()
 
 app.listen(PORT, () => {
     console.log(`Express está escuchando en el puerto http://localhost:${PORT}`)
